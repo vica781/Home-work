c9385020868a4111b95ffb63a172d62dd4b33c30
Victoria Kalenik <vk781@hotmail.com>
2023-03-25 16:23:08 +0000

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+//creating a number array
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//to log all elements of the 'nembers' array to console
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}
+
+let randomSelection;
+
+
